8508d1ca4e5c9b9937a3fa29ab1120beb67fe9d3
Maria Robles <mariarobles1979@gmail.com>
2022-03-08 14:33:39 +0100

    Terminado a falta de validación

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+var numA = () => parseInt(document.getElementById("numberA").value);
+var numB = () => parseInt(document.getElementById("numberB").value);
+
+//operaciones
+
+var sumNumber = () => numA() + numB();
+var restNumber = ()  => numA() - numB();
+var multiplyNumber = () => numA() * numB();
+var divNumber = ()  => numA() / numB();
+
+//metemos resultados en el DOM
+
+var resultadoSum = () => document.getElementById("result").innerText = sumNumber();
+var resultadoRest = () => document.getElementById("result").innerText = restNumber();
+var resultadoMulti = () => document.getElementById("result").innerText = multiplyNumber();
+var resultadoDiv = () => document.getElementById("result").innerText = divNumber();
+
+//evento para que al dar click haga la función de meter el resultado
+
+document.getElementById("sumar").addEventListener("click", resultadoSum );
+document.getElementById("restar").addEventListener("click", resultadoRest);
+document.getElementById("multiplicar").addEventListener("click", resultadoMulti);
+document.getElementById("dividir").addEventListener("click", resultadoDiv);
+
+ 
+
+/*function validateSum (){
+if (isNaN(numA())|| isNan(numB())){
+   document.getElementById("result").innerText = "Rellena los campos numéricos";
+}else{
+  resultadoSum();
+}
+}*/
+
+//Lo he intentado con el isNaN y con el === NaN pero no dice que "isNan is not defined"
+
